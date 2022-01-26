0cb9d795644688481211202048d630fac37851ef
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2022-01-26 10:59:06 -0300

    ensure EmailValidator returns true if validator returns true

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,5 +1,14 @@
+class EmailValidator{
+  isValid(email){
+    return true
+  }
+}
+
 describe('Email Validator', () => {
-  test('started test email validator', () => {
+  test('should returns true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@mail.com')
+    expect(isEmailValid).toBe(true)
 
   })
 })
