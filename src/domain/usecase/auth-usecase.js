e2d8ab328088ee5512fe303c49a8749baef1d8f0
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2022-03-17 16:32:03 -0300

    simplify code

--- src/domain/usecase/auth-usecase.js
+++ src/domain/usecase/auth-usecase.js
@@ -1,5 +1,4 @@
 const { MissingParamError } = require('../../utils/errors')
-
 module.exports = class AuthUseCase {
   constructor (loadUserByEmailRepository, encrypter, tokenGenerator) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
@@ -14,16 +13,12 @@ module.exports = class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
-
     const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) {
-      return null
+    const isValid = user && await this.encrypter.compare(password, user.password)
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id)
+      return accessToken
     }
-    const isValid = await this.encrypter.compare(password, user.password)
-    if (!isValid) {
-      return null
-    }
-    const accessToken = await this.tokenGenerator.generate(user.id)
-    return accessToken
+    return null
   }
 }
