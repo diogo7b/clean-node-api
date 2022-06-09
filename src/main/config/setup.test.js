3a41253c28627dbbddd2929c24382450c4da2fb4
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2022-06-09 15:05:25 -0300

    should enabled CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -2,12 +2,23 @@ const request = require('supertest')
 const app = require('./app')
 
 describe('App Setup', () => {
-    test('test Should disable x-powered-by header', async () => {
-        app.get('/test_spoweredby', (req, res) => {
-            res.send('x-powered-by')
-        })
-
-        const res = await request(app).get('/test_spoweredby')
-        expect(res.headers['x-powered-by']).toBeUndefined()
+  test('test Should disable x-powered-by header', async () => {
+    app.get('/test_spoweredby', (req, res) => {
+      res.send('x-powered-by')
     })
+
+    const res = await request(app).get('/test_spoweredby')
+    expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enabled CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('cors')
+    })
+
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
