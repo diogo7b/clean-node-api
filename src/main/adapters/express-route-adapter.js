4103595cf9065b335a023abd238ad1d0aaa25e86
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2022-06-26 22:02:51 -0300

    add express router adapter

---
+++ src/main/adapters/express-route-adapter.js
@@ -0,0 +1,12 @@
+module.export = class ExpressRouteAdapter {
+  static adapt (router) {
+    return async (request, response) => {
+      const httpRequest = {
+        body: request.body
+      }
+
+      const httpResponse = await router.route(httpRequest)
+      response.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
