d609fb202ed015a806b982eef1d5efa44222fdc5
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2022-07-06 22:12:22 -0300

    adjusted import error

--- src/main/adapters/express-route-adapter.js
+++ src/main/adapters/express-route-adapter.js
@@ -1,4 +1,4 @@
-module.export = class ExpressRouteAdapter {
+module.exports = class ExpressRouteAdapter {
   static adapt (router) {
     return async (request, response) => {
       const httpRequest = {
